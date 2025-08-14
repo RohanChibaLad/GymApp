3602a87d309226102f060737bb0eb82d9afaac3f
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-08-14 10:44:28 +0100

    Made the model better and added comments

--- services/Users/UserApp/models.py
+++ services/Users/UserApp/models.py
@@ -1,21 +1,39 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.db import models # Importing Django models
+from django.contrib.auth.models import AbstractUser # Importing AbstractUser to create a custom user model
+from django.core.validators import MinValueValidator, MaxValueValidator, RegexValidator # Importing validators for model fields
 
-# Create your models here.
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    firstName = models.CharField(max_length=30, blank=True)
-    lastName = models.CharField(max_length=30, blank=True)
-    weight = models.FloatField(null=True, blank=True)
-    height = models.FloatField(null=True, blank=True)
-    dateOfBirth = models.DateField(null=True, blank=True)
-    phoneNumber = models.CharField(max_length=15, null=True, blank=True)    
+phone_validator = RegexValidator(r"^\+?[1-9]\d{1,14}$", "Enter a valid phone number.") # Regex for phone number validation
 
-    def __str__(self):
+class User(AbstractUser): # Custom User model inheriting from AbstractUser
+    email = models.EmailField(unique=True) #Email field with unique constraint
+    date_of_birth = models.DateField(null=True, blank=True) #Date of birth field that can be null or blank
+    
+    #Phone number field with regex validation
+    phone_number = models.CharField( 
+        max_length=15, 
+        null=True, 
+        blank=True, 
+        validators=[phone_validator]
+    )
+
+    # Weight field with decimal validation
+    weight = models.DecimalField(
+        null=True, 
+        blank=True, 
+        max_digits=5, 
+        decimal_places=2,
+        validators=[MinValueValidator(0)])
+    
+    #Height field with positive integer validation
+    height = models.PositiveIntegerField( 
+        null=True, 
+        blank=True,
+        validators=[MinValueValidator(40), MaxValueValidator(300)]
+    )
+    def __str__(self): # String representation of the User model
         return self.username
 
-    class Meta:
+    class Meta: # Meta class for User model
         verbose_name = "User"
         verbose_name_plural = "Users"
         ordering = ["username"]
-        indexes = [models.Index(fields=["email"])]
