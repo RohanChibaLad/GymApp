58dcf0fc7a6cc244f1ff5b7ecbc83908b9ea2508
rohanchiba-lad <sc21rcl@leeds.ac.uk>
2025-08-29 17:21:06 +0100

    Docker stuff is set up, but server cant be accessed now

--- services/Users/UserSite/settings.py
+++ services/Users/UserSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+#Docker changes
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-zhw498=)!%oq12*3tfa(tsabtf+%y*q_@@&%ru!0o6p%b*l&qz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "user-app",
+    "localhost"
+    
+]
 
 # Application definition
 
